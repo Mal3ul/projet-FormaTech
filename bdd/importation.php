ce2a7136ddfea365f11c7e5820eb31f49f6bedcf
Imad CHAMKHI <imad.chamkhi@mediaschool.me>
2023-12-18 17:53:58 +0100

    Refactor import.php pour utiliser des instructions préparées pour l’insertion de données

--- bdd/importation.php
+++ bdd/importation.php
@@ -1,37 +1,21 @@
 <?php
-$pdo = new PDO('mysql:host=localhost;dbname=your_database;charset=utf8mb4', 'your_username', 'your_password');
-$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+include 'datas.php';
+include 'config.php';
 
-// Insert data into the "employees" table
-foreach ($formations as $formation) {
-    $sql = "INSERT INTO formations (name, durée, abréviation, RNCP_niveau, nombre_module) VALUES (:name, :duree, :abreviation, :niveau, :module)";
-    $stmt = $pdo->prepare($sql);
+try {
 
-    $stmt->bindParam(':name', $formation['nom']);
-    $stmt->bindParam(':duree', $formation['duree']);
-    $stmt->bindParam(':abreviation', $formation['abreviation']);
-    $stmt->bindParam(':niveau', $formation['niveau']);
-    $stmt->bindParam(':module', true); // Assuming you want to set this to true for all records
-
-    $stmt->execute();
-}
-
-// Insert data into the "modules" table
-foreach ($modules as $module) {
-    // First, check if the formation exists
-    $formationName = $module['formation'];
-    $formationId = $pdo->query("SELECT id FROM formations WHERE name = '$formationName'")->fetchColumn();
-
-    if ($formationId) {
-        // If the formation exists, insert the module
-        $sql = "INSERT INTO modules (name, durée, module_id) VALUES (:name, :duree, :moduleId)";
+    foreach ($formations as $formation) {
+        $sql = "INSERT INTO formations (nom, abreviation, duree, niveau ) VALUES (:nom, :abreviation, :duree, :niveau)";
         $stmt = $pdo->prepare($sql);
-
-        $stmt->bindParam(':name', $module['nom']);
-        $stmt->bindParam(':duree', $module['duree']);
-        $stmt->bindParam(':moduleId', $formationId);
-
-        $stmt->execute();
+        $stmt->execute([
+            ':nom' => $formation['nom'], 
+            ':abreviation' => $formation['abreviation'],
+            ':duree' => $formation['duree'],
+            ':niveau' => $formation['niveau'],
+        ]);
     }
+
+    echo "Data inserted successfully.";
+} catch (PDOException $e) {
+    echo "Error: " . $e->getMessage();
 }
-?>
